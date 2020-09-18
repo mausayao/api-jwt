f7c0f50690c0436023a61518a981acf93b9cc591
mausayao <mauricio.sayao@gmail.com>
2020-09-18 10:41:39 -0400

    Adicionado validacao

--- status/serializers.py
+++ status/serializers.py
@@ -12,3 +12,20 @@ class StatusSerializer(serializers.ModelSerializer):
             'content',
             'image'
         )
+
+    # def validate_content(sef, value):
+    #     if len(value) > 10000:
+    #         raise serializers.ValidationError("This is too long")
+    #     return value
+
+    def validate(self, data):
+        content = data.get('content', None)
+
+        if content == "":
+            content = None
+        
+        image = data.get('image', None)
+
+        if content is None and image is None:
+            raise serializers.ValidationError('Content or image is required')
+        return data
